bf43f19f05ea16fdcb9d1599f570951d5a08382f
linhaidong3 <linhaidong3@jd.com>
2022-03-13 00:11:17 +0800

    feat: 初始化代码

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'semi': true,
+  'singleQuote': true,
+  'jsxSingleQuote': true,
+  'printWidth': 200,
+  'tabWidth': 2,
+  'trailingComma': "all",
+  'arrowParens': "always",
+  'overrides': [
+    {
+      'files': '*.{js,jsx,tsx,ts,scss,json,html}',
+      'options': {
+        'tabWidth': 2,
+      },
+    },
+  ]
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # doga-design
-doga 是基于 Doga Design 设计体系的 UI 组件库，主要用于研发企业级产品
+
+### doga 是基于 Doga Design 设计体系的 UI 组件库，主要用于研发企业级产品
